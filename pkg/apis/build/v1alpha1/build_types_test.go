6c90f7ae9a2a24205b48377fa9fe7cf28a73bc94
Matt Moore <mattmoor@google.com>
2018-06-19 00:28:52 +0000

    Add GetCondition to BuildStatus.

--- pkg/apis/build/v1alpha1/build_types_test.go
+++ pkg/apis/build/v1alpha1/build_types_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/knative/build/pkg/buildtest"
@@ -67,6 +68,10 @@ func TestBuildConditions(t *testing.T) {
 
 	if len(rev.Status.Conditions) != 1 {
 		t.Fatalf("Unexpected Condition length; want 1, got %d", len(rev.Status.Conditions))
+	}
+
+	if got, want := rev.Status.GetCondition(foo.Type), foo; !reflect.DeepEqual(got, want) {
+		t.Errorf("GetCondition() = %v, want %v", got, want)
 	}
 
 	// Add a second condition.
