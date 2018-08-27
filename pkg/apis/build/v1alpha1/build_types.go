6abbb1460dcb2ab647fd29dea22b2df7effd1e30
Shash Reddy <sreddy@pivotal.io>
2018-08-27 16:04:18 -0400

    Fix typo

--- pkg/apis/build/v1alpha1/build_types.go
+++ pkg/apis/build/v1alpha1/build_types.go
@@ -72,7 +72,7 @@ type BuildSpec struct {
 	// +optional
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 
-	// Time after which the build times out. Defaults to 10minutes.
+	// Time after which the build times out. Defaults to 10 minutes.
 	//  Go's ParseDuration documentation for expected format: https://golang.org/pkg/time/#ParseDuration
 	Timeout string `json:"timeout,omitempty"`
 }
