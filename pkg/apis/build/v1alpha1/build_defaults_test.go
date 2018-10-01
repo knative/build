d2381d0eafb87463aa4c7e3fbb21fdee9116b872
Shash Reddy <sreddy@pivotal.io>
2018-10-01 14:33:30 -0400

    Add tests

--- pkg/apis/build/v1alpha1/build_defaults_test.go
+++ pkg/apis/build/v1alpha1/build_defaults_test.go
@@ -2,6 +2,9 @@ package v1alpha1
 
 import (
 	"testing"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestSetDefault(t *testing.T) {
@@ -12,5 +15,23 @@ func TestSetDefault(t *testing.T) {
 	}
 	if emptyBuild.Spec.Timeout.Duration != DefaultTimeout {
 		t.Errorf("Expect build timeout to be set")
+	}
+}
+
+func TestAlreadySetDefault(t *testing.T) {
+	setAccountName := "test-account-name"
+	setTimeout := metav1.Duration{Duration: 20 * time.Minute}
+	setDefaultBuild := &Build{
+		Spec: BuildSpec{
+			ServiceAccountName: setAccountName,
+			Timeout:            setTimeout,
+		},
+	}
+	setDefaultBuild.SetDefaults()
+	if setDefaultBuild.Spec.ServiceAccountName != setAccountName {
+		t.Errorf("Expect build.spec.serviceaccount name not to be overridden; but got %s", setDefaultBuild.Spec.ServiceAccountName)
+	}
+	if setDefaultBuild.Spec.Timeout != setTimeout {
+		t.Errorf("Expect build.spec.timeout not to be overridden; but got %s", setDefaultBuild.Spec.Timeout)
 	}
 }
